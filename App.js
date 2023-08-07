cdd5ee34f6cff2bbb48757aad2d98585a8a05db7
Baz125 <103266154+Baz125@users.noreply.github.com>
2023-08-07 09:45:06 +0200

    Connected Firebase DB

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ShoppingLists from './components/ShoppingLists';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  const firebaseConfig = {
+  apiKey: "AIzaSyB35PnDm8te1b0TdhGOeynrcRKN2kemuSA",
+  authDomain: "listapp-92a14.firebaseapp.com",
+  projectId: "listapp-92a14",
+  storageBucket: "listapp-92a14.appspot.com",
+  messagingSenderId: "147914270614",
+  appId: "1:147914270614:web:f8ace1a23c69357a0fdfb7",
+  measurementId: "G-JFFXHS4VRH"
+  };
+  
+  const app = initializeApp(firebaseConfig);
+
+  const db = getFirestore(app);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="ShoppingLists"
+      >
+        <Stack.Screen
+          name="ShoppingLists"
+          // component={ShoppingLists}
+        >
+          {props => <ShoppingLists db={db} {...props} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -18,3 +47,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+export default App;
